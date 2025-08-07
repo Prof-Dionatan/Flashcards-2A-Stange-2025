2cfb9125668d28ff66420f55f4bfecdf2c7308f1
Prof-Dionatan <dionatan.markiu@escola.pr.gov.br>
2025-08-07 23:27:39 +0000

    carões no js

--- app.js
+++ app.js
@@ -3,4 +3,17 @@ function criaCartao(categoria, pergunta, resposta){
     const cartao = document.createElement('article')
     cartao.className = 'cartao'
 
+    cartao.innerHTML = `
+        <div class="cartao_conteudo">
+            <h3>${categoria}</h3>
+            <div class="pergunta">
+                ${pergunta}
+            </div>
+            <div class="resposta">
+                ${resposta}
+            </div>
+        </div>
+    `
+
+    container.appendChild(cartao)
 }
